8aedf716854b2b02141da58e0546e36483b7ce64
Mack McQuen <mackmcquen@comcast.net>
2020-03-24 23:05:22 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+add = 5 + 4
 end
 
 def subtraction(num1, num2)
-
+subtract = 10 - 5
 end
 
 def division(num1, num2)
-
+divide = 50 / 2
 end
 
 def multiplication(num1, num2)
-
+multiply = 4 * 30
 end
 
 def modulo(num1, num2)
-
+remainder = (34).remainder(5)
 end
 
 def square_root(num)
-
+square = Math.sqrt(81)
 end
