02f8440dc5edea42171036786057e9eb06bf3a06
Mack McQuen <mackmcquen@comcast.net>
2020-03-24 23:01:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,15 @@ add = 5 + 4
 end
 
 def subtraction(num1, num2)
-
+subtract = 10 - 5
 end
 
 def division(num1, num2)
-
+divide = 50 / 2
 end
 
 def multiplication(num1, num2)
-
+multiply = 4 * 30
 end
 
 def modulo(num1, num2)
